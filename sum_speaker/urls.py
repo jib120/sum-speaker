e99b8e69cc3ef16998aad2526d040e33f3e05f50
Hyunji Cha <hyunji3190@gmail.com>
2018-05-29 19:47:16 +0900

    Delete urls.py

--- sum_speaker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.urls import path
-from . import views
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('index.html', views.index, name='index'),
-    path('search/view.html', views.search, name='search_view'),
-]
